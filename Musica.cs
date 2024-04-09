4772ab7ba39843c85b12eda5972e3adf11cde62c
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-04-08 23:33:41 -0300

    Criar classe música
    Criado a classe de música, no arquivo "Musica.cs".

---
+++ Musica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace screen_sound_alura_oop
+{
+    public class Musica
+    {
+        public required string Name { get; set; }
+        public required string Artist { get; set; }
+        public required TimeSpan Duration { get; set; }
+        public required bool Available { get; set; }
+        public int Segundos => Duration.Seconds + (Duration.Minutes * 60);
+        public string Resumo => $"{Name} do artista {Artist}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var musica = new Musica
+            {
+                Artist = "O gay",
+                Name = "The incredibles",
+                Duration = new TimeSpan(days: 0, hours: 0, minutes: 2, seconds: 30),
+                Available = true,
+            };
+
+
+            Console.WriteLine(musica.Resumo);
+            Console.WriteLine($"Duração em segundos: {musica.Segundos}");
         }
     }
 }
