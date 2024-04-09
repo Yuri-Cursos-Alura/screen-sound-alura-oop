2df8009f8cdb3cb57d5e0310fd5476b5181ff126
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-04-08 22:26:38 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace screen_sound_alura_oop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
